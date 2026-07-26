3939b0435db6c9787e5c9ad3f21c9c028f43ff3b
BRGoldino <76222248+BRGoldino@users.noreply.github.com>
2026-07-26 18:54:53 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dunnas-v59';
+const CACHE_NAME = 'dunnas-v61';
 const ASSETS = ['./dunnas.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
